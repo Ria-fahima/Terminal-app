2bd657042ed15b3ba3555360bbfe553e1faf79f3
fahimagithub <fahima96khan@gmail.com>
2022-09-23 18:58:31 +1000

    Added error handling in the Coupon Game

--- main.py
+++ main.py
@@ -5,31 +5,32 @@ import random as r
 Total = 0
 print(figlet_format("ZEIN" , font= "standard"))
 
-member = input("Are you a member of Zein? (y/n): ")
+member = input("Are you a member of Zein? (y/n): ").strip()
   
 while member != "y" and member != "n":
     print("Enter y or n!")
-    member = input("Are you a member of Zein? (y/n): ")
+    member = input("Are you a member of Zein? (y/n): ").strip()
 if member ==    "y":
-    user_name = input("Please enter your user name: ")
+    user_name = input("Please enter your user name: ").strip()
+    new_member = "no"
     with open("members.json") as f:
         store_data = json.load(f)
         for member in store_data:
             if user_name == member['name']:
-                password = input("Please enter your password: ")
+                password = input("Please enter your password: ").strip()
                 if password == member['password']:
                     print("Welcome to our website!", member['name'])
-                    new_member = "no"
+                    
                 else:
                     print("Wrong password! You are not out member!") 
-                    new_member = "no"      
+                         
 
 else:
     print("For our new member, you can get 5 percent discount on your first purchase!") 
-    confirmation = input("Are you interested? (y/n):")
+    confirmation = input("Are you interested? (y/n):").strip()
     while confirmation != "y" and confirmation != "n":
         print("Enter y or n!")
-        confirmation = input("Are you still interested? (y/n): ")
+        confirmation = input("Are you still interested? (y/n): ").strip()
     if confirmation == "y":
         print("please fill up some information about yourself.")
         new_user = input("Write Your preferred username:")
@@ -72,7 +73,7 @@ def available_products(p):
             options()
             break    
 def options():
-    option = input("Do you want to check other catagories? (y/n): ")
+    option = input("Do you want to check other catagories? (y/n): ").strip()
     if option == 'y':
         main()
     elif option == 'n':
@@ -117,17 +118,38 @@ def main():
              
 main()  
 print(f"Your collections are: {cart}")  
-
-def delete_cart():
-    delete_item = input("Please write the product name to remove.")
-    cart.remove(delete_item.upper())
-    print(f"Your collections are: {cart}")
-    del_choice = input("Do you want to delete more? (y/n): ")
+class InputError(Exception):
+    pass
+def get_deleteItem():
+    print(f"Your current cart collections are: {cart}")
+    delete_item = input("Please write the product name to remove.").strip().upper()
+    for w in range(len(cart)):
+        if cart[w] != delete_item:
+            raise InputError
+        else:
+            cart.remove(delete_item) 
+            break   
+    return cart     
+def delete_extra():
+    del_choice = input("Do you want to delete more? (y/n): ").strip()
     if del_choice == "y":
         delete_cart()
-        
-    else:
+    elif del_choice == "n":
         edit_cart()
+    else:
+        print("Please enter y or n!")   
+        return delete_extra() 
+
+def delete_cart():
+    while True:
+        try:
+            r = get_deleteItem()
+            break
+        except InputError:
+            print("Please enter the valid product name")    
+    
+    print(f"Your collections are: {cart}")
+    delete_extra()
 
 def payment(m,z):
     
@@ -139,24 +161,44 @@ def payment(m,z):
                 if cart[o].lower() == l['Name'].lower():
                     z += l['Price']   
         return z  
-        
-def game():
-    
-    print("let's play a coupon game!")
+def user_guessGame():
     secret_num = r.randint(1,20)
-    
     user_guess = int(input("Guess a number between 1 to 20: "))
-    if user_guess == secret_num:
+    if not user_guess in range(1,21):
+        raise RangeError
+    elif user_guess == secret_num:
         print("Congratulations! You got 10 percent discount")
         game = "yes"
-           
     else:   
         print("Better luck next time! :) ")  
-        game = 0       
-    return game                           
-def edit_cart():
+        game = 0     
+    
+    return game   
+def game(): 
+    print("let's play a coupon game!")
+    
+    while True:
+        try:
+            y = user_guessGame()
+            break
+        except RangeError:
+            print("Input is out of range - must be between 1 and 20")
+        except ValueError:
+            print("Input must be an Integer!")    
+    return y     
+def modify_cart():
+    modify = input("Do you want to add or delete (add / delete)?: ")
+    if modify == "add":
+        main()
+        edit_cart()
+    elif modify == "delete":
+        delete_cart()
+    else: 
+        print("Please write add or delete!") 
+        return modify_cart()  
 
-    proceed = input("Do you want to proceed for checkout (y/n)?:  ")
+def edit_cart():
+    proceed = input("Do you want to proceed for checkout (y/n)?:  ").strip()
     if proceed == "y":
         bag_sum = payment("bags.json",Total)
         cloth_sum =payment("cloths.json",Total)
@@ -165,21 +207,22 @@ def edit_cart():
         print("Original price: $", total)
         if new_member == "yes":
             total = total - (total * 0.05)
+            print("After Membership discount total amount is $", total)
         game_winner = game()
         if game_winner == "yes":
             total = total - (total * 0.1)  
         print(f"Your Final collections are: {cart}") 
         print("Your total amount is: $", total)
 
-    else:
+    elif proceed == "n":
         print("Edit your cart")
-        modify = input("Do you want to add or delete (add / delete)?: ")
-        if modify == "add":
-            main()
-            edit_cart()
-        elif modify == "delete":
-            delete_cart()
+        modify_cart()   
+    else:
+        print("Please write y or n!")    
+        return edit_cart()    
 edit_cart()
+
+
 
 
 
